cbf3bf926122b62ef96cfaae04617286ba83de23
Jeriko <jcarrico1985@live.com>
2017-12-22 14:28:25 -0600

    added beginning of logic for scrolling to anchor points when nav item is clicked. will not work yet. added minor styling changes. navbar will now be fixed to screen. mobile-first design. next up is desktop and tablet break-points.

--- logic.js
+++ logic.js
@@ -4,11 +4,30 @@
 
   let navbar = document.querySelector('.navbar');
   let hamburger = document.getElementById('hamburger');
+  let navItem = document.getElementsByClassName('nav-item');
+  let footerChevron = document.getElementById('footer-chevron');
 
-  hamburger.onclick = function() {
-    console.log('onclick worked');
-    console.log(navbar.classList);
+  hamburger.onclick = () => {
     navbar.classList.toggle('hidden');
+    navbar.style.position = "fixed";
+    navbar.style.top = "0";
   };
+
+  footerChevron.onclick = () => {
+    window.scrollTo(0,0);
+  };
+
+  navItem.onClick = () => {
+    console.log(this.href);
+    window.scrollTo(getOffset(this.href).left, getOffset(this.href).top);
+  };
+
+  function getOffset(el) {
+    el = el.getBoundingClientRect();
+    return {
+      left: el.left + window.scrollX,
+      top: el.top + window.scrollY
+    }
+  }
 
 })();
