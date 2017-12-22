66477549652a027db6f7bdea8aa2289b78ac9d44
Jeriko <jcarrico1985@live.com>
2017-12-22 13:25:52 -0600

    added image for down-arrow. nearly completed layout of application. added click logic for hamburger menu expansion and contracting. next is anchor tag navigation

--- logic.js
+++ logic.js
@@ -2,6 +2,13 @@
 
 (() => {
 
+  let navbar = document.querySelector('.navbar');
+  let hamburger = document.getElementById('hamburger');
 
+  hamburger.onclick = function() {
+    console.log('onclick worked');
+    console.log(navbar.classList);
+    navbar.classList.toggle('hidden');
+  };
 
 })();
